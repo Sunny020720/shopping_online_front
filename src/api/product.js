cb8ae1df962cdb64f81b2a3181ea7b09987340c4
Sunny020720 <1538052461@qq.com>
2023-08-11 20:22:08 +0800

    api-搜索商品列表数据

---
+++ src/api/product.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+// 获取搜索商品列表的数据
+export const getProList = (obj) => {
+  const { categoryId, goodsName, page } = obj
+  return request.get('/goods/list', {
+    params: {
+      categoryId,
+      goodsName,
+      page
+    }
+  })
+}
