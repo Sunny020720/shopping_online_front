98f9e04a8123856f0ab48c2ee795df0e64b22702
Sunny020720 <1538052461@qq.com>
2023-08-14 20:15:21 +0800

    api——获取商品详情&评论数据

--- src/api/product.js
+++ src/api/product.js
@@ -10,3 +10,20 @@ export const getProList = (obj) => {
     }
   })
 }
+// 获取商品详情数据
+export const getProDetail = (goodsId) => {
+  return request.get('/goods/detail', {
+    params: {
+      goodsId
+    }
+  })
+}
+// 获取商品评价
+export const getProComment = (goodsId, limit) => {
+  return request.get('/comment/listRows', {
+    params: {
+      goodsId,
+      limit
+    }
+  })
+}
