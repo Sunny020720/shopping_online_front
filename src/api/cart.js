7ea5db6f5054823ddc01f329bafcc54fdb936c13
Sunny020720 <1538052461@qq.com>
2023-08-20 22:26:38 +0800

    api-获取购物车商品列表

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,10 +1,15 @@
 import request from '@/utils/request'
-// 添加商品到购物车
 
+// 添加商品到购物车
 export const addCart = (goodsId, goodsNum, goodsSkuId) => {
   return request.post('/cart/add', {
     goodsId, // =>商品id iphone8
     goodsSkuId, // =>商品规格id 红色的iphone8
     goodsNum
   })
+}
+
+// 获取购物车商品列表
+export const getCartList = () => {
+  return request.get('/cart/list')
 }
