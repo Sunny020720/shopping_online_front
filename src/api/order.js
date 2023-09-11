b8bd0bdd8bbd5a3a77a59a96ec8b5952fbd1cf1f
Sunny020720 <1538052461@qq.com>
2023-09-12 01:16:16 +0800

    Api-订单结算-提交订单并支付

--- src/api/order.js
+++ src/api/order.js
@@ -14,3 +14,14 @@ export const checkOrder = (mode, obj) => {
     }
   })
 }
+// 提交订单
+export const submitOrder = (mode, obj) => {
+  return request.post('/checkout/submit', {
+    mode,
+    delivery: 10, // 物流方式  配送方式 (10快递配送 20门店自提)
+    couponId: 0, // 优惠券 id
+    payType: 10, // 余额支付
+    isUsePoints: 0, // 是否使用积分
+    ...obj
+  })
+}
